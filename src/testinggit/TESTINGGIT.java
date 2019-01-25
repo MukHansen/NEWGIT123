8a8a625a29393cfed3482842bb64cb71fa470c7c
MukHansen <Fubar93@gmail.com>
2019-01-25 13:03:08 +0100

    Testing

---
+++ src/testinggit/TESTINGGIT.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testinggit;
+
+/**
+ *
+ * @author Bruger
+ */
+public class TESTINGGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("TEEEEEEEEST GIIIIIIIIIIIT MOTHERFUCKER");
+        // TODO code application logic here
+    }
+    
+}
